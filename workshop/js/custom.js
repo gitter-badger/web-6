43eba49322ea5ee822fac05d44d19dad1baf79b0
Vojtěch Dubrovský <v.dubr@hotmail.com{template .feature.Text}v.dubr@hotmail.com>
2015-03-04 22:42:31 +0100

    # update gisworkshop web

--- workshop/js/custom.js
+++ workshop/js/custom.js
@@ -1,13 +1,15 @@
-$(document).on('click', '.panel-heading span.clickable', function(e){
+$(document).on('click', '.panel-heading .clickable', function(e){
     var $this = $(this);
-	if(!$this.hasClass('panel-collapsed')) {
-		$this.parents('.panel').find('.panel-body').slideDown();
-		$this.addClass('panel-collapsed');
-		$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+    var icon = $this.parents('.panel-heading').find('span.clickable')
+    var colapsedContent = $this.parents('.panel').find('.panel-body')
+	if(!icon.hasClass('panel-collapsed')) {
+    colapsedContent.slideDown();
+		icon.addClass('panel-collapsed');
+		icon.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
 	} else {
-		$this.parents('.panel').find('.panel-body').slideUp();
-		$this.removeClass('panel-collapsed');
-		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+    colapsedContent.slideUp();
+		icon.removeClass('panel-collapsed');
+		icon.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
 	}
 })
 
