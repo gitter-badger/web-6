c1368fff4bdf44c0055a90d4cb28ecd23ea69682
Vojtěch Dubrovský <v.dubr@hotmail.com{template .feature.Text}v.dubr@hotmail.com>
2015-10-20 00:40:42 +0200

    init webu pro gishack2015

--- mapito/assets/script/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	 $("#play").mouseenter(function() {
-        $('img',this).attr("src", "assets/img/play_h.svg");
-     }).mouseleave(function() {
-        $('img',this).attr("src", "assets/img/play.svg");
-     });
-});
